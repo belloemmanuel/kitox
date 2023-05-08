c50d5b363bb73596070654a3c0eb55378271736f
belloemmanuel <belloemmanuel02@gmail.com>
2023-05-08 11:31:25 +0100

    js form updated

--- kitox.js
+++ kitox.js
@@ -5,9 +5,17 @@ regularPrice.addEventListener('keyup',calcDiscount)
 
 function calcDiscount(){
 calcDiscount = parseInt(regularPrice.value) * 0.5
-if(regularPrice.value == '' || isNaN(regularPrice.value)){
+if(regularPrice.value == ''){
     totalPrice.innerHTML = '<em>Please Enter a Regular Price</em>'
-} else {totalPrice.innerHTML = `$${calcDiscount}`}
+    totalPrice.style.color = "red"
+    totalPrice.style.fontSize = "14px"
+} else if(isNaN(regularPrice.value)){
+    totalPrice.innerHTML = '<em>Please Enter a Valid Price</em>'
+    totalPrice.style.color = "red"
+}
+    else {totalPrice.innerHTML = `$${calcDiscount}`
+    totalPrice.style.color = "white"
+}
 }
 
  
