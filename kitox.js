62158b8f7f8a5ff3b07ab0c9b5f0c8a55aa230da
belloemmanuel <belloemmanuel02@gmail.com>
2023-05-08 02:57:55 +0100

    changes made

---
+++ kitox.js
@@ -0,0 +1,13 @@
+let regularPrice = document.getElementById('regularprice');
+let totalPrice = document.getElementById('totalprice');
+
+regularPrice.addEventListener('keyup',calcDiscount)
+
+function calcDiscount(){
+calcDiscount = parseInt(regularPrice.value) * 0.5
+if(regularPrice.value == '' || regularPrice.value == NaN ){
+    totalPrice.innerHTML = '<em>Please Enter a Regular Price</em>'
+} else {totalPrice.innerHTML = `$${calcDiscount}`}
+}
+
+ 
